87172d7b75a094a4ac504e09a16091bd8f86ee53
shlominave <s1170@motzkine.ort.org.il>
2020-10-29 01:23:12 +0200

    COMMIT NUM 2

--- app/src/main/java/com/example/shlomixo/MainActivity.java
+++ app/src/main/java/com/example/shlomixo/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.shlomixo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+ArrayList<Integer> player1=new ArrayList<>();
+    ArrayList<Integer> player2=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void processgame(View view){
+        Button choosesbtn=(Button)view;
+        choosesbtn.setText("X");
+        choosesbtn.setBackgroundResource(R.color.xocolor);
     }
 }
