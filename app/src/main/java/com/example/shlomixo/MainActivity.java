4e652a6f544df1a59810669a66abc39367a0239a
shlominave <s1170@motzkine.ort.org.il>
2020-10-30 12:08:35 +0200

    COMMIT NUM 3

--- app/src/main/java/com/example/shlomixo/MainActivity.java
+++ app/src/main/java/com/example/shlomixo/MainActivity.java
@@ -16,9 +16,36 @@ ArrayList<Integer> player1=new ArrayList<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void processgame(View view){
-        Button choosesbtn=(Button)view;
+    public void processgame(View view) {
+        Button choosesbtn = (Button) view;
         choosesbtn.setText("X");
         choosesbtn.setBackgroundResource(R.color.xocolor);
+        choosesbtn.setEnabled(false);
+        int cellnum = 0;
+        if (choosesbtn.getId() == R.id.btn1)
+            cellnum = 1;
+        if (choosesbtn.getId() == R.id.btn2)
+            cellnum = 2;
+        if (choosesbtn.getId() == R.id.btn3)
+            cellnum = 3;
+        if (choosesbtn.getId() == R.id.btn4)
+            cellnum = 4;
+        if (choosesbtn.getId() == R.id.btn5)
+            cellnum = 5;
+        if (choosesbtn.getId() == R.id.btn6)
+            cellnum = 6;
+        if (choosesbtn.getId() == R.id.btn7)
+            cellnum = 7;
+        if (choosesbtn.getId() == R.id.btn8)
+            cellnum = 8;
+        if (choosesbtn.getId() == R.id.btn9)
+            cellnum = 9;
+        player1.add(cellnum);
+    }
+    public void  mobilechooses()
+    {
+        
+
+
     }
 }
